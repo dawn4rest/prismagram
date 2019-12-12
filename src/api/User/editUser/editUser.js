dd6cc4890c1c8a4fa6b8d94975cd28dde5e3179e
dawn4rest <dawn4rest@gmail.com>
2019-12-12 17:35:50 +0900

    feat. 3.11 editUser seeUser Resolver

---
+++ src/api/User/editUser/editUser.js
@@ -0,0 +1,18 @@
+import { isAuthenticated } from "../../../middlewares";
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+  Mutation: {
+    editUser: async (_, args, { request }) => {
+      isAuthenticated(request);
+
+      const { user } = request;
+      const { username, email, firstName, lastName, bio } = args;
+
+      return prisma.updateUser({
+        where: { id: user.id },
+        data: { username, email, firstName, lastName, bio }
+      });
+    }
+  }
+};
